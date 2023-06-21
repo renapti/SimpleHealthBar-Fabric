835d62b8ddffb32e03a66e37fb51e0eb5857d2f9
Lanfix <raphael.chapotard@outlook.fr>
2023-06-21 15:10:32 +0200

    Created 1.19.3 version (asked on curseforge)

---
+++ remappedSrc/fr/lanfix/simplehealthbar/SimpleHealthBar.java
@@ -0,0 +1,13 @@
+package fr.lanfix.simplehealthbar;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SimpleHealthBar implements ModInitializer {
+
+    public static final String MOD_ID = "simple-health-bar";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
